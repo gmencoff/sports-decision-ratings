8ab5e3a606ff635ae9f4fde5871b59fb8bdf9db8
George Mencoff <george.mencoff@gmail.com>
2026-02-13 21:24:28 -0500

    make home page dynamic:

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,8 @@
 import { TransactionCard } from '@/components/TransactionCard';
 import { getTransactions } from './actions/transactions';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const { data: transactions } = await getTransactions();
 
